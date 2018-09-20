3191102da0ff584da9616504b5c1398242fc2122
Zachary Boyd <zachary@zacharyboyd.nyc>
2018-09-19 22:36:22 -0400

    Adds missing components for index

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
-export { default as Face } from "./Faces/Face";
 export { default as Command } from "./Commands/Command";
+export { default as CommandOptions } from "./Commands/CommandOptions";
+export { default as ICommandService } from "./Commands/ICommandService";
 export { default as ICommandType } from "./Commands/ICommandType";
-export { default as RunCondition } from "./Commands/RunCondition";
+export { default as IDetectionService } from "./Detection/IDetectionService";
 export { default as DetectionOptions } from "./Detection/DetectionOptions";
 export { default as EigenFaceRecognizerOptions } from "./Detection/EigenFaceRecognizerOptions";
+export { default as Face } from "./Faces/Face";
+export { default as IFaceManagementService } from "./Faces/IFaceManagementService";
+export { default as RunCondition, RunConditionType } from "./Commands/RunCondition";
 export { default as Status } from "./Detection/Status";
+
 export { 
     IncorrectRunConditionTypeToSetFaces
 } from "./Errors";

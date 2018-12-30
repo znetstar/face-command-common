037f5cedd6ebe05ac401a9eb1b38b442809cee2c
Zachary Boyd <zachary@zacharyboyd.nyc>
2018-12-30 11:45:21 -0800

    Adds an property for loading autostart faces.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## [1.2.0] - 2018-12-30
+### Added
+- Adds an property for loading autostart faces.
+
 ## [1.1.0] - 2018-12-28
 ### Added
 - Adds a class that can generate random components for testing.
--- src/Detection/DetectionOptions.ts
+++ src/Detection/DetectionOptions.ts
@@ -10,8 +10,9 @@ export default class DetectionOptions {
      * @param frequency - List of faces to scan for during detection.
      * @param eigenFaceRecognizerOptions - How frequently capture from the capture source (e.g. webcam) in milliseconds.
      * @param faces - Options that will be passed to the EigenFaceRecognizer instance.
+     * @param autostartFaces - Adds the faces that would be loaded on start.
      */
-    constructor(public frequency: number, public eigenFaceRecognizerOptions: EigenFaceRecognizerOptions, public faces: Face[] = []) {
+    constructor(public frequency: number, public eigenFaceRecognizerOptions: EigenFaceRecognizerOptions, public faces: Face[] = [], public autostartFaces: boolean = false) {
         
     }
 }
